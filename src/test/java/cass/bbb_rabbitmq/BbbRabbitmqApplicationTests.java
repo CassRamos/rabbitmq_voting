632be89f7af392f712338888403a5220bc28efd3
Cass <cassianoleaoramos@gmail.com>
2024-12-04 21:10:52 -0300

    Initial project config

---
+++ src/test/java/cass/bbb_rabbitmq/BbbRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package cass.bbb_rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbbRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
